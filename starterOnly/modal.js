7f4169daf2d1b07e47a1e9ba94b6add6f637c664
Marie Rodiet <marie.rodiet@gmail.com>
2022-01-13 17:55:51 +0100

    comments added to code

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -17,30 +17,26 @@ function editNav() {
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
 
-
-
-
-//when cross is clicked, form needs to disappear
+//le formulaire disparait lorsque la CROIX est cliquée
 let close = document.querySelector(".close");
 close.addEventListener("click", function () {
   modalbg.style.display = "none";
 });
 
+//les champs de saisies du formulaires
 const first = document.querySelector('input[name="first"]');
 const last = document.querySelector('input[name="last"]');
 const email = document.querySelector('input[name="email"]');
 const birthdate = document.querySelector('input[name="birthdate"]');
 const quantity = document.querySelector('input[name="quantity"]');
-const locationError = document.querySelector("#location-error");
-const conditionError = document.querySelector("#condition-error");
 
+//modification spécifiques du message d'erreur pour chaque input du formulaire
 first.addEventListener('invalid', function (event) {
   if (event.target.validity.valueMissing) {
     event.target.setCustomValidity('Veuillez entrer 2 caractères ou plus pour le champ du prénom.');
@@ -70,7 +66,7 @@ quantity.addEventListener('invalid', function (event) {
   }
 });
 
-
+//faire un reset du message d'erreur afin que le formulaire soit validé après correction
 document.querySelectorAll('input').forEach(item => {
   item.addEventListener('change', function (event) {
     event.target.setCustomValidity('');
@@ -78,6 +74,9 @@ document.querySelectorAll('input').forEach(item => {
 });
 
 
+//les messages d'erreurs des deux derniers input du formulaire
+const locationError = document.querySelector("#location-error");
+const conditionError = document.querySelector("#condition-error");
 
 function validateData(event) {
   event.preventDefault();
@@ -85,12 +84,14 @@ function validateData(event) {
   const entries = formData.entries();
   const data = Object.fromEntries(entries);
   console.log(data);
+  //data.location doit être selectionné
   if (!data.location) {
     locationError.textContent = "Merci de selectionner le tournoi auquel vous souhaitez participer cette année!";
     locationError.style.fontSize = "16px";
     locationError.style.fontWeight = "900";
 
   }
+  //checkbox1 doit être selectionné
   else if (!document.querySelector("#checkbox1").checked) {
     conditionError.textContent = "Merci de lire et d'accepter les conditions d'utilisation!";
     conditionError.style.fontSize = "16px";
@@ -99,6 +100,7 @@ function validateData(event) {
     conditionError.style.fontWeight = "900";
   }
   else {
+    //le formulaire passe les tests : il disparait après que les informations soient stockées dans finalData
     //use JSON.stringify to convert data into a string that can now be sent to a server
     finalData = JSON.stringify(data);
     console.log("finalData");
